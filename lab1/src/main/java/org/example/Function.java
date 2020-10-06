e4a0d20757efe70967bdd71d4e28061ca2239559
Julia0212 <yulia.samartseva@gmail.com>
2020-10-06 20:58:23 +0300

    add skeleton of the project

---
+++ lab1/src/main/java/org/example/Function.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Function implements Runnable {
+
+  private int x;
+  public Function(int x) {
+    this.x = x;
+  }
+
+  @Override
+  public void run() {
+
+  }
+
+}
