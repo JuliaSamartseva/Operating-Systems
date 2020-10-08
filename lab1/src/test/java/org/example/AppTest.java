6923882ca966147ca68898f1bd366f5896db72a6
Julia0212 <yulia.samartseva@gmail.com>
2020-10-08 14:52:56 +0300

    add listeners for the computation results, shutdown by escape key

--- lab1/src/test/java/org/example/AppTest.java
+++ lab1/src/test/java/org/example/AppTest.java
@@ -1,14 +1,12 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
-
+import java.io.IOException;
 import org.junit.Test;
-import spos.lab1.demo.IntOps;
 
 public class AppTest {
 
   @Test
-  public void shouldAnswerWithTrue() {
-    assertTrue(true);
+  public void shouldAnswerWithTrue() throws IOException, InterruptedException {
   }
+
 }
