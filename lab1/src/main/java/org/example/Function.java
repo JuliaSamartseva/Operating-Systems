0e55621076ebe73b144830cff3ee3e1b2541d52e
Julia0212 <yulia.samartseva@gmail.com>
2020-10-12 23:59:27 +0300

    code reformatting

--- lab1/src/main/java/org/example/Function.java
+++ lab1/src/main/java/org/example/Function.java
@@ -3,24 +3,21 @@ package org.example;
 import java.io.IOException;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
-import java.util.logging.FileHandler;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
 public class Function implements Runnable {
 
   private final int input;
   private final Computation function;
   private volatile boolean running = true;
-  private List<Listener> listeners = new ArrayList<>();
-  private PipedOutputStream outputStream = new PipedOutputStream();
-  private static Logger log = Logger.getLogger(Manager.class.getName());
+  private final List<Listener> listeners = new ArrayList<>();
+  private final PipedOutputStream outputStream = new PipedOutputStream();
+  private static final Logger log = Logger.getLogger(Manager.class.getName());
 
-  public Function(int input, Computation function, PipedInputStream inputStream) throws IOException {
+  public Function(int input, Computation function, PipedInputStream inputStream)
+      throws IOException {
     this.input = input;
     this.function = function;
     inputStream.connect(outputStream);
@@ -34,7 +31,7 @@ public class Function implements Runnable {
         int result = function.applyFunction(input);
         log.info(
             "The function was computed. Passing the "
-                + Integer.toString(result)
+                + result
                 + " result from the function to the output stream.");
         outputStream.write(result);
         notifyListeners();
