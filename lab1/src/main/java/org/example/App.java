51bc698f0737d950c6a235f09d6adc3d867f37fc
Julia0212 <yulia.samartseva@gmail.com>
2020-10-14 15:51:46 +0300

    add modes inside the manager

--- lab1/src/main/java/org/example/App.java
+++ lab1/src/main/java/org/example/App.java
@@ -7,8 +7,7 @@ import spos.lab1.demo.IntOps;
 public class App {
   public static void main(String[] args) throws IOException, InterruptedException {
     LogManager.getLogManager().reset();
-    System.out.println("Usual computations with shutdown key: ");
-    Manager manager = new Manager(10, 10, IntOps::funcF, SampleFunctions::sampleTwo);
-    manager.startComputations();
+    Manager manager = new Manager(1, 1, SampleFunctions::sampleF, SampleFunctions::sampleG);
+    manager.startComputations(Mode.SHUTDOWN_PROMPT);
   }
 }
--- lab1/src/main/java/org/example/SampleFunctions.java
+++ lab1/src/main/java/org/example/SampleFunctions.java
@@ -5,15 +5,27 @@ import java.util.concurrent.TimeUnit;
 
 public class SampleFunctions {
 
-  public static int sampleOne(int x) throws InterruptedException, UndefinedResultException {
-    SECONDS.sleep(20);
-    if (x == 50) throw new UndefinedResultException("Undefined result");
+  public static int sampleF(int x) throws InterruptedException, UndefinedResultException {
+    if (x == 1) SECONDS.sleep(5);
+    else if (x == 2) {
+      SECONDS.sleep(1);
+      return 0;
+    }
+    else if (x == 3) SECONDS.sleep(15);
+    else if (x == 4) SECONDS.sleep(8);
+    else if (x == 50) throw new UndefinedResultException("Undefined result");
     return x * 10;
   }
 
-  public static int sampleTwo(int x) throws InterruptedException, UndefinedResultException {
-    SECONDS.sleep(2);
-    if (x == 20) throw new UndefinedResultException("Undefined result");
-    return 15;
+  public static int sampleG(int x) throws InterruptedException, UndefinedResultException {
+    if (x == 1) SECONDS.sleep(3);
+    else if (x == 2) {
+      SECONDS.sleep(1);
+      return 0;
+    }
+    else if (x == 3) SECONDS.sleep(25);
+    else if (x == 4) SECONDS.sleep(7);
+    else if (x == 50) throw new UndefinedResultException("Undefined result");
+    return 15 + x;
   }
 }
