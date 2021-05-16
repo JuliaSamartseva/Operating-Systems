51204f628c0f8f779425edf8d8f7cb7467fa04b7
Julia <yulia.samartseva@gmail.com>
2021-05-16 22:53:42 +0300

    initial simple lexer implementation

---
+++ LexerCpp/LexerCpp/LexerCpp.cpp
@@ -0,0 +1,19 @@
+// LexerCpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include "Lexer.h"
+
+int main()
+{
+    auto code = "int x == 3444.54";
+
+    Lexer lex(code);
+    for (auto token = lex.next();
+        not token.is_one_of(Token::Kind::End, Token::Kind::Unexpected);
+        token = lex.next()) {
+        std::cout << Token::toString(token.kind()) << " |" << token.lexeme()
+            << "|\n";
+    }
+}
