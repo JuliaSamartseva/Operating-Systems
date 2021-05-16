4f523e0d5360bd1fd9a1bdcb3b85c84a1744452f
Julia <yulia.samartseva@gmail.com>
2021-05-16 21:25:19 +0300

    add jflex generated lexer

--- Lexer/src/main/java/org/example/App.java
+++ Lexer/src/main/java/org/example/App.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
+  public static void main(String[] args) throws FileNotFoundException {
+    FileReader fileReader = new FileReader("test.txt");
+    Lexer cLexer = new Lexer(fileReader);
+    int idx = 0;
+    System.out.println("List of tokens:");
+    System.out.println("Output format - (start index, token class, representation)");
+    while (true) {
+      try {
+        Type type = cLexer.yylex();
+        if (type == Type.EOF) {
+          break;
+        }
+        int len = cLexer.yytext().length();
+        String target = String.format("(%d, %s, \"%s\")", idx, type.name(), cLexer.yytext());
+        System.out.println(target);
+        idx += len;
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
   }
 }
---
+++ Lexer/src/main/java/org/example/Type.java
@@ -0,0 +1,33 @@
+package org.example;
+public enum Type {
+  // numeric constants
+  INTEGER_LITERAL,
+  FLOATING_POINT_LITERAL,
+  // literals and symbolic constants
+  CHARACTER_LITERAL,
+  STRING,
+  // preprocessor directives
+  DEFINE_LINE,
+  // comments
+  COMMENT,
+  // keywords
+  KEYWORD,
+  // identifiers
+  IDENTIFIER,
+  // operators
+  OPERATOR,
+  // punctuation marks
+  EOF,
+  COMMA,
+  DOT,
+  SEMICOLON,
+  RIGHT_BRACKET,
+  LEFT_BRACKET,
+  LEFT_PARENTHESIS,
+  RIGHT_PARENTHESIS,
+  RIGHT_BRACE,
+  LEFT_BRACE,
+  PRETREATMENT_LINE,
+  NEW_LINE,
+  WHITE_SPACE
+}
